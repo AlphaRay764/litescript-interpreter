3a7c7902a9f451f15c75c9532173160e63a986f7
Spencer <spencerbetchey@gmail.com>
2026-03-28 18:29:26 -0500

    Add Token class with token types and structure

---
+++ LiteScript/src/Token.java
@@ -0,0 +1,36 @@
+public class Token {
+
+    // All the possible types a token can be
+    public enum Type {
+        // Literals
+        NUMBER, IDENTIFIER,
+
+        // Operators
+        PLUS, MINUS, MULTIPLY, DIVIDE,
+
+        // Comparison
+        EQUALS, EQUALS_EQUALS, GREATER, LESS,
+
+        // Grouping
+        LPAREN, RPAREN,
+
+        // Keywords
+        IF, THEN, ELSE,
+
+        // Control
+        EOF  // End of input
+    }
+
+    public final Type type;    // Kind of token 
+    public final String value; // Thetext it came from
+
+    public Token(Type type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Token(" + type + ", " + value + ")";
+    }
+}
